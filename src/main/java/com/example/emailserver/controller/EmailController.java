92d6e9e47862b8d215fbe8c4ffa612d4858f11bf
Hun0914 <bkh010914@naver.com>
2025-05-12 16:39:46 +0900

    feat : Controller 개발

--- src/main/java/com/example/emailserver/controller/EmailController.java
+++ src/main/java/com/example/emailserver/controller/EmailController.java
@@ -1,12 +1,18 @@
 package com.example.emailserver.controller;
 
+import com.example.emailserver.service.SignService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-public class EamilController {
+@RequiredArgsConstructor
+@Controller
+public class EmailController {
 
-    
+    private final SignService signService;
+
     @PostMapping(value = "/request-sign-up")
     public ResponseEntity<?> requestSignUp(@RequestBody SignUpRequestDto signUpRequestDto){
         return ResponseEntity.ok(signService.sendCode(signUpRequestDto));
